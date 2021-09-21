bf97c283e2ffe6799153e895568346262cfbbd6b
chevtastic <christopherchevrier@gmail.com>
2021-09-21 00:35:03 -0400

    first commit for input02

---
+++ input02.py
@@ -0,0 +1,6 @@
+firstName = input("What is your first name? ")
+capitalFirst = firstName.capitalize()
+lastName = input("what is your last name? ")
+capitalLast = lastName.capitalize()
+
+print(f"Your name is {capitalLast}, {capitalFirst} {capitalLast}.")
