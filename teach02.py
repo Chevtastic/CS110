607a2396e9ec55e6c59c7064773a4da651752e0b
chevtastic <christopherchevrier@gmail.com>
2021-09-21 23:53:46 -0400

    done with teach02.py

--- teach02.py
+++ teach02.py
@@ -1,4 +1,5 @@
 
+print("Please enter the following information:\n")
 
 fName = input("First Name: ")
 lName = input("Last Name: ")
@@ -7,20 +8,26 @@ phNum = input("Phone Number: ")
 
 jobTitle = input("Job Title: ")
 idNum = input("ID Number: ")
+
+# Stretch details
 hair = input("Hair Color: ")
 eyes = input("Eye Color: ")
 month = input("Start Month: ")
-training = input("Training complete? (Y/N): ")
+training = input("Training complete (Yes/No)?: ")
 
-
-
+#print user ID Card
 print(f"The ID Card is:")
 print(f"----------------------------------------")
 print(f"{lName.upper()}, {fName.capitalize()}")
-print(f"Job title: {jobTitle.title()}")
+print(f"{jobTitle.title()}")
 print(f"ID: {idNum}\n")
 
 print(f"{email.lower()}")
-print(f"{phNum}\n")
-#print(f"({phNum[:3]}) {phNum[3:7]}-{phNum[7:]}")
+#print(f"{phNum}\n")
+
+#Phone number formatting to add parenthesis and dash 
+print(f"({phNum[:3]}) {phNum[3:7]}-{phNum[7:]}\n")
+
+print(f"{'Hair: ' + hair.capitalize():<25} {'Eyes: ' + eyes.capitalize()}")
+print(f"{'Month: ' + month.capitalize():<25} {'Training: ' + training.capitalize()}")
 print(f"----------------------------------------")
